dc8fcd4895811dd506e000bd82b3959caa22f37e
TruffleWorm <rishay.raghav00@gmail.com>
2023-03-30 12:08:36 +0530

    added a default city

--- index.js
+++ index.js
@@ -14,7 +14,39 @@ const apiKey = process.env.API_KEY;
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.render('home');
+  const defaultCity = 'Delhi';
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&appid=${apiKey}&units=metric`
+  https.get(url, (response) => {
+    response.on('data', (weather) => {
+      if (response.statusCode == 200) {
+        const data = JSON.parse(weather);
+        const temp = data.main.temp;
+        const feels_like = data.main.feels_like;
+        const temp_min = data.main.temp_min;
+        const temp_max = data.main.temp_max;
+        let description = data.weather[0].main.toLowerCase();
+        switch (description) {
+          case 'clouds':
+            description = 'cloudy'
+            break;
+          case 'haze':
+            description = 'hazy'
+            break;
+          default:
+            break;
+        }
+        const weatherData = {
+          city: data.name,
+          temp: temp,
+          feels_like: feels_like,
+          temp_min: temp_min,
+          temp_max: temp_max,
+          description: description
+        }
+        res.render('default', { weatherData: weatherData });
+      }
+    })
+  })
 })
 
 app.post('/', (req, res) => {
@@ -29,8 +61,15 @@ app.post('/', (req, res) => {
         const temp_min = data.main.temp_min;
         const temp_max = data.main.temp_max;
         let description = data.weather[0].main.toLowerCase();
-        if (description == 'clouds') {
-          description = 'cloudy';
+        switch (description) {
+          case 'clouds':
+            description = 'cloudy'
+            break;
+          case 'haze':
+            description = 'hazy'
+            break;
+          default:
+            break;
         }
         const weatherData = {
           city: data.name,
@@ -42,7 +81,7 @@ app.post('/', (req, res) => {
         }
         res.render('weather', { weatherData: weatherData });
       }
-      else {
+      if (response.statusCode === 404) {
         res.render('404');
       }
     })
