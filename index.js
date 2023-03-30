bbe0f55a11e53126eb3b091441b2907427487a04
TruffleWorm <rishay.raghav00@gmail.com>
2023-03-30 11:04:02 +0530

    improved description

--- index.js
+++ index.js
@@ -14,41 +14,41 @@ const apiKey = process.env.API_KEY;
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-    res.render('home');
+  res.render('home');
 })
 
 app.post('/', (req, res) => {
-    const city = req.body.city;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-    https.get(url, (response) => {
-        response.on('data', (weather) => {
-            if (response.statusCode == 200) {
-                const data = JSON.parse(weather);
-                const temp = data.main.temp;
-                const feels_like = data.main.feels_like;
-                const temp_min = data.main.temp_min;
-                const temp_max = data.main.temp_max;
-                let description = data.weather[0].main.toLowerCase();
-                if (description == 'clouds') {
-                    description = 'cloudy';
-                }
-                const weatherData = {
-                    city: data.name,
-                    temp: temp,
-                    feels_like: feels_like,
-                    temp_min: temp_min,
-                    temp_max: temp_max,
-                    description: description
-                }
-                res.render('weather', { weatherData: weatherData });
-            }
-            else {
-                res.render('404');
-            }
-        })
+  const city = req.body.city;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+  https.get(url, (response) => {
+    response.on('data', (weather) => {
+      if (response.statusCode == 200) {
+        const data = JSON.parse(weather);
+        const temp = data.main.temp;
+        const feels_like = data.main.feels_like;
+        const temp_min = data.main.temp_min;
+        const temp_max = data.main.temp_max;
+        let description = data.weather[0].main.toLowerCase();
+        if (description == 'clouds') {
+          description = 'cloudy';
+        }
+        const weatherData = {
+          city: data.name,
+          temp: temp,
+          feels_like: feels_like,
+          temp_min: temp_min,
+          temp_max: temp_max,
+          description: description
+        }
+        res.render('weather', { weatherData: weatherData });
+      }
+      else {
+        res.render('404');
+      }
     })
+  })
 })
 
 app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`);
+  console.log(`Listening at http://localhost:${port}`);
 })
