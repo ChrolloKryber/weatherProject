24fa5a62662029bb56ef880ffd7160a4b0d4cf18
TruffleWorm <rishay.raghav00@gmail.com>
2023-03-31 21:27:11 +0530

    edge case fixed

--- index.js
+++ index.js
@@ -68,30 +68,35 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   const city = req.body.city;
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
-  https.get(url, (response) => {
-    response.on('data', (weather) => {
-      if (response.statusCode == 200) {
-        const data = JSON.parse(weather);
-        const temp = data.main.temp;
-        const feels_like = data.main.feels_like;
-        const temp_min = data.main.temp_min;
-        const temp_max = data.main.temp_max;
-        let description = switchDescription(data.weather[0].main.toLowerCase());
-        const weatherData = {
-          city: data.name,
-          temp: temp,
-          feels_like: feels_like,
-          temp_min: temp_min,
-          temp_max: temp_max,
-          description: description
+  if (city !== "") {
+    https.get(url, (response) => {
+      response.on('data', (weather) => {
+        if (response.statusCode == 200) {
+          const data = JSON.parse(weather);
+          const temp = data.main.temp;
+          const feels_like = data.main.feels_like;
+          const temp_min = data.main.temp_min;
+          const temp_max = data.main.temp_max;
+          let description = switchDescription(data.weather[0].main.toLowerCase());
+          const weatherData = {
+            city: data.name,
+            temp: temp,
+            feels_like: feels_like,
+            temp_min: temp_min,
+            temp_max: temp_max,
+            description: description
+          }
+          res.render('weather', { weatherData: weatherData });
         }
-        res.render('weather', { weatherData: weatherData });
-      }
-      if (response.statusCode === 404) {
-        res.render('404');
-      }
+        if (response.statusCode === 404) {
+          res.render('404');
+        }
+      })
     })
-  })
+  }
+  else {
+    res.render('404');
+  }
 })
 
 app.listen(port, () => {
